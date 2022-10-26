6c20a3b1ac2992b711c2ffec5f41993dea91dbce
javier-rosario-rov <116303180+javier-rosario-rov@users.noreply.github.com>
2022-10-26 17:58:51 -0300

    Add files via upload

--- 02-math.js
+++ 02-math.js
@@ -1,28 +1,19 @@
 function sum(a, b) {
   return a + b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
-  if (b === 0)
-    return null;
-
+  if (b === 0) return null;
   return a / b;
 }
-
 function average(a, b, c) {
   return ((a + b + c) / 3);
-
 }
-
 module.exports = {
-
   sum,
   multiply,
   divide,
   average,
-
 };
